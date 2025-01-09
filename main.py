eed07a48e5500a79fb62176cb11b1b3d506eab16
Adriano França <adriano.franca@discente.ufma.br>
2025-01-09 11:25:17 -0300

    add: alterações js

--- main.py
+++ main.py
@@ -27,145 +27,166 @@ def bbox_to_bytes(bbox_array):
 # Inicializar o modelo de detecção de face Haar Cascade
 face_cascade = cv2.CascadeClassifier(cv2.samples.findFile(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'))
 
-# JavaScript para criar o stream de vídeo
-def video_stream():
+
+# Função para carregar o JavaScript com Promise
+def setup_javascript():
     js = Javascript('''
-    var video;
-    var div = null;
-    var stream;
-    var captureCanvas;
-    var imgElement;
-    var labelElement;
-    
-    var pendingResolve = null;
-    var shutdown = false;
-    
-    function removeDom() {
-        stream.getVideoTracks()[0].stop();
-        video.remove();
-        div.remove();
-        video = null;
-        div = null;
-        stream = null;
-        imgElement = null;
-        captureCanvas = null;
-        labelElement = null;
-    }
-    
-    function onAnimationFrame() {
-      if (!shutdown) {
-        window.requestAnimationFrame(onAnimationFrame);
-      }
-      if (pendingResolve) {
-        var result = "";
-        if (!shutdown) {
-          captureCanvas.getContext('2d').drawImage(video, 0, 0, 640, 480);
-          result = captureCanvas.toDataURL('image/jpeg', 0.8)
+        function setup_javascript() {
+           return new Promise(resolve => {
+               var video;
+               var div = null;
+               var stream;
+               var captureCanvas;
+               var imgElement;
+               var labelElement;
+
+               var pendingResolve = null;
+               var shutdown = false;
+
+               function removeDom() {
+                   if(stream){
+                    stream.getVideoTracks()[0].stop();
+                   }
+                   if(video){
+                       video.remove();
+                   }
+                   if (div){
+                       div.remove();
+                   }
+                   video = null;
+                   div = null;
+                   stream = null;
+                   imgElement = null;
+                   captureCanvas = null;
+                   labelElement = null;
+               }
+
+
+               function onAnimationFrame() {
+                   if (!shutdown) {
+                       window.requestAnimationFrame(onAnimationFrame);
+                   }
+                   if (pendingResolve) {
+                       var result = "";
+                       if (!shutdown) {
+                           captureCanvas.getContext('2d').drawImage(video, 0, 0, 640, 480);
+                           result = captureCanvas.toDataURL('image/jpeg', 0.8)
+                       }
+                       var lp = pendingResolve;
+                       pendingResolve = null;
+                       lp(result);
+                   }
+               }
+
+               async function createDom() {
+                   if (div !== null) {
+                       return stream;
+                   }
+
+                   div = document.createElement('div');
+                   div.style.border = '2px solid black';
+                   div.style.padding = '3px';
+                   div.style.width = '100%';
+                   div.style.maxWidth = '600px';
+                   document.body.appendChild(div);
+
+                   const modelOut = document.createElement('div');
+                   modelOut.innerHTML = "<span>Status:</span>";
+                   labelElement = document.createElement('span');
+                   labelElement.innerText = 'No data';
+                   labelElement.style.fontWeight = 'bold';
+                   modelOut.appendChild(labelElement);
+                   div.appendChild(modelOut);
+
+                   video = document.createElement('video');
+                   video.style.display = 'block';
+                   video.width = div.clientWidth - 6;
+                   video.setAttribute('playsinline', '');
+                   video.onclick = () => { shutdown = true; };
+                   stream = await navigator.mediaDevices.getUserMedia(
+                       {video: { facingMode: "environment"}});
+                   div.appendChild(video);
+
+                   imgElement = document.createElement('img');
+                   imgElement.style.position = 'absolute';
+                   imgElement.style.zIndex = 1;
+                   imgElement.onclick = () => { shutdown = true; };
+                   div.appendChild(imgElement);
+
+                   const instruction = document.createElement('div');
+                   instruction.innerHTML =
+                       '<span style="color: red; font-weight: bold;">' +
+                       'When finished, click here or on the video to stop this demo</span>';
+                   div.appendChild(instruction);
+                   instruction.onclick = () => { shutdown = true; };
+
+                   video.srcObject = stream;
+                   await video.play();
+
+                   captureCanvas = document.createElement('canvas');
+                   captureCanvas.width = 640;
+                   captureCanvas.height = 480;
+                   window.requestAnimationFrame(onAnimationFrame);
+
+                   return stream;
+               }
+
+
+              async function stream_frame(label, imgData) {
+                   if (shutdown) {
+                      removeDom();
+                      shutdown = false;
+                      return '';
+                   }
+
+                   var preCreate = Date.now();
+                   stream = await createDom();
+
+                   var preShow = Date.now();
+                   if (label != "") {
+                      labelElement.innerHTML = label;
+                   }
+
+                   if (imgData != "") {
+                      var videoRect = video.getClientRects()[0];
+                      imgElement.style.top = videoRect.top + "px";
+                      imgElement.style.left = videoRect.left + "px";
+                      imgElement.style.width = videoRect.width + "px";
+                      imgElement.style.height = videoRect.height + "px";
+                      imgElement.src = imgData;
+                   }
+
+                   var preCapture = Date.now();
+                   var result = await new Promise(function(resolve, reject) {
+                      pendingResolve = resolve;
+                   });
+                   shutdown = false;
+
+                   return {'create': preShow - preCreate,
+                           'show': preCapture - preShow,
+                           'capture': Date.now() - preCapture,
+                           'img': result};
+              }
+             window.stream_frame = stream_frame
+            resolve("ready");
+          });
+
         }
-        var lp = pendingResolve;
-        pendingResolve = null;
-        lp(result);
-      }
-    }
-    
-    async function createDom() {
-      if (div !== null) {
-        return stream;
-      }
-
-      div = document.createElement('div');
-      div.style.border = '2px solid black';
-      div.style.padding = '3px';
-      div.style.width = '100%';
-      div.style.maxWidth = '600px';
-      document.body.appendChild(div);
-      
-      const modelOut = document.createElement('div');
-      modelOut.innerHTML = "<span>Status:</span>";
-      labelElement = document.createElement('span');
-      labelElement.innerText = 'No data';
-      labelElement.style.fontWeight = 'bold';
-      modelOut.appendChild(labelElement);
-      div.appendChild(modelOut);
-           
-      video = document.createElement('video');
-      video.style.display = 'block';
-      video.width = div.clientWidth - 6;
-      video.setAttribute('playsinline', '');
-      video.onclick = () => { shutdown = true; };
-      stream = await navigator.mediaDevices.getUserMedia(
-          {video: { facingMode: "environment"}});
-      div.appendChild(video);
-
-      imgElement = document.createElement('img');
-      imgElement.style.position = 'absolute';
-      imgElement.style.zIndex = 1;
-      imgElement.onclick = () => { shutdown = true; };
-      div.appendChild(imgElement);
-      
-      const instruction = document.createElement('div');
-      instruction.innerHTML = 
-          '<span style="color: red; font-weight: bold;">' +
-          'When finished, click here or on the video to stop this demo</span>';
-      div.appendChild(instruction);
-      instruction.onclick = () => { shutdown = true; };
-      
-      video.srcObject = stream;
-      await video.play();
-
-      captureCanvas = document.createElement('canvas');
-      captureCanvas.width = 640; 
-      captureCanvas.height = 480;
-      window.requestAnimationFrame(onAnimationFrame);
-      
-      return stream;
-    }
-    async function stream_frame(label, imgData) {
-      if (shutdown) {
-        removeDom();
-        shutdown = false;
-        return '';
-      }
-
-      var preCreate = Date.now();
-      stream = await createDom();
-      
-      var preShow = Date.now();
-      if (label != "") {
-        labelElement.innerHTML = label;
-      }
-            
-      if (imgData != "") {
-        var videoRect = video.getClientRects()[0];
-        imgElement.style.top = videoRect.top + "px";
-        imgElement.style.left = videoRect.left + "px";
-        imgElement.style.width = videoRect.width + "px";
-        imgElement.style.height = videoRect.height + "px";
-        imgElement.src = imgData;
-      }
-      
-      var preCapture = Date.now();
-      var result = await new Promise(function(resolve, reject) {
-        pendingResolve = resolve;
-      });
-      shutdown = false;
-      
-      return {'create': preShow - preCreate, 
-              'show': preCapture - preShow, 
-              'capture': Date.now() - preCapture,
-              'img': result};
-    }
-    ''')
+       setup_javascript();
+        ''')
     display(js)
+
+
 
 # Função para processar cada frame do vídeo
 def video_frame(label, bbox):
-    data = eval_js('stream_frame("{}", "{}")'.format(label, bbox))
+    data = eval_js('window.stream_frame("{}", "{}")'.format(label, bbox))
     return data
 
 # Iniciar o stream de vídeo da webcam
-video_stream()
-
+print("Setting up javascript...")
+js_ready = eval_js('setup_javascript()')  # Aguarda o sinal "ready" do JavaScript
+print("Javascript is ready:", js_ready)
 # Label para o vídeo
 label_html = 'Reconhecimento Facial'
 
